442fecd1c80c78d7fff18908d80376bbb388e276
Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>
2025-01-25 20:39:41 +0100

    Initial gradle commit

---
+++ app/src/main/java/org/keylogger/app/App.java
@@ -0,0 +1,9 @@
+package org.keylogger.app;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
