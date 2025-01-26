2a4a931366c1b97ebe855d4bb1eb74423da91448
Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>
2025-01-26 16:18:37 +0100

    Key combination implementation

--- app/src/main/java/org/keylogger/utils/KeyManager.java
+++ app/src/main/java/org/keylogger/utils/KeyManager.java
@@ -6,7 +6,12 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 
 public class KeyManager implements NativeKeyListener {
+	private short hotKeyflag = 0x00;
+	private static final short MASK_CTRL = 1 << 0;
+	private static final short MASK_ALT = 1 << 1;
 
+
+	@Override
     public void nativeKeyPressed(NativeKeyEvent e) {
 
 		// If the VK_ESCAPE (escape) key is pressed, tries to unregister the native hook.
@@ -16,16 +21,30 @@ public class KeyManager implements NativeKeyListener {
             } catch (NativeHookException nativeHookException) {
                 nativeHookException.printStackTrace();
             }
-        }
+        } else if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL) {
+			hotKeyflag |= MASK_CTRL;
+		} else if (e.getKeyCode() == NativeKeyEvent.VC_ALT) {
+			hotKeyflag |= MASK_ALT;
+		} 
+		
+		if (hotKeyflag == (MASK_CTRL | MASK_ALT)) {
+			System.out.println("Hotkey pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+		}
 	}
 
+	@Override
 	public void nativeKeyReleased(NativeKeyEvent e) {
 		System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 
-		// TODO: Implement the logic to check the released key
+		if (e.getKeyCode() == NativeKeyEvent.VC_CONTROL) {
+			hotKeyflag &= ~MASK_CTRL;
+		} else if (e.getKeyCode() == NativeKeyEvent.VC_ALT) {
+			hotKeyflag &= ~MASK_ALT;
+		}
 
 	}
 
+	@Override
 	public void nativeKeyTyped(NativeKeyEvent e) {
 		System.out.println("Key Typed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 	}
--- app/src/main/java/org/keylogger/utils/MouseManager.java
+++ app/src/main/java/org/keylogger/utils/MouseManager.java
@@ -6,22 +6,27 @@ import com.github.kwhat.jnativehook.GlobalScreen;
 
 public class MouseManager implements NativeMouseInputListener {
 
+    @Override
     public void nativeMouseClicked(NativeMouseEvent e) {
         System.out.println("Mouse Clicked: " + e.getButton());
     }
 
+    @Override
     public void nativeMousePressed(NativeMouseEvent e) {
         System.out.println("Mouse Pressed: " + e.getButton());
     }
 
+    @Override
     public void nativeMouseReleased(NativeMouseEvent e) {
         System.out.println("Mouse Released: " + e.getButton());
     }
 
+    @Override
     public void nativeMouseMoved(NativeMouseEvent e) {
         System.out.println("Mouse Moved: " + e.getX() + ", " + e.getY());
     }
 
+    @Override
     public void nativeMouseDragged(NativeMouseEvent e) {
         System.out.println("Mouse Dragged: " + e.getX() + ", " + e.getY());
     }
